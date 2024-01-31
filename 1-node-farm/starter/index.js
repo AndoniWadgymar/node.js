7f6d9b5ae08791ad6f970689d19eb1b7c553ba23
Andoni Wadgymar <andowii@gmail.com>
2024-01-30 20:12:57 -0600

    Callback Heal

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,13 +1,27 @@
 const fs = require("fs");
+
 const { text } = require("stream/consumers");
 
 // Blocking, synchronous way
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
 fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written");
 
 // Non-blocking, synchronous way
-fs.readFile("./txt/start.txt", "utf-8", (error, data) => {
-  console.log(data);
+fs.readFile("txt/start.txt", "utf-8", (error, data1) => {
+  console.log(data1);
+  fs.readFile(`txt/${data1}.txt`, "utf-8", (error, data2) => {
+    console.log(data2);
+    fs.readFile(`txt/append.txt`, "utf-8", (error, data3) => {
+      console.log(data3);
+      fs.writeFile(
+        "./txt/final.txt",
+        `${data2}\n${data3}`,
+        "utf-8",
+        (error) => {
+          console.log("Your file has been written!");
+        }
+      );
+    });
+  });
 });
